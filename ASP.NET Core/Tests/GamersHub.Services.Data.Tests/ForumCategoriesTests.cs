f2d344325225c751fdc49da139c4898b17598c80
StoyanRangelov <49279325+StoyanRangelov@users.noreply.github.com>
2020-04-16 13:20:39 +0300

    Finished ForumCategoriesTests

---
+++ ASP.NET Core/Tests/GamersHub.Services.Data.Tests/ForumCategoriesTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using GamersHub.Data;
+using GamersHub.Data.Models;
+using GamersHub.Data.Repositories;
+using GamersHub.Services.Data.ForumCategories;
+using GamersHub.Services.Mapping;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace GamersHub.Services.Data.Tests
+{
+    [TestFixture]
+    public class ForumCategoriesTests
+    {
+        private EfRepository<ForumCategory> repository;
+        private ForumCategoriesService forumCategoriesService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            this.repository = new EfRepository<ForumCategory>(new ApplicationDbContext(options.Options));
+            this.forumCategoriesService = new ForumCategoriesService(this.repository);
+            AutoMapperConfig.RegisterMappings(typeof(TestForumCategory).Assembly);
+        }
+
+
+        [Test]
+        public async Task TestGetByNameAndForumId()
+        {
+            await this.repository.AddAsync(new ForumCategory
+                {ForumId = 1, Category = new Category {Name = "category"}});
+
+            for (int i = 2; i < 5; i++)
+            {
+                await this.repository.AddAsync(new ForumCategory
+                    {ForumId = i, Category = new Category {Name = Guid.NewGuid().ToString() } });
+            }
+
+            await this.repository.SaveChangesAsync();
+
+            var forumCategory = this.forumCategoriesService.GetByNameAndForumId<TestForumCategory>("category", 1);
+
+            Assert.AreEqual(1, forumCategory.ForumId);
+            Assert.AreEqual("category", forumCategory.CategoryName);
+        }
+    }
+
+    public class TestForumCategory : IMapFrom<ForumCategory>
+    {
+        public string CategoryName { get; set; }
+
+        public int ForumId { get; set; }
+    }
+}
