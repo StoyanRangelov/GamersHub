59240bad611f25517fae86de6d2256945f17a0d8
StoyanRangelov <49279325+StoyanRangelov@users.noreply.github.com>
2020-04-17 19:20:04 +0300

    Scaffolded Account/Manage/_Layout.cshtml and modified it to look better

---
+++ ASP.NET Core/Web/GamersHub.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GamersHub.Common;
+using GamersHub.Data.Models;
+using GamersHub.Services.Data.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GamersHub.Web.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IUsersService _usersService;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IUsersService usersService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _usersService = usersService;
+        }
+
+        public string Username { get; set; }
+
+        [Display(Name = "Current Gaming Experience")]
+        [EnumDataType(typeof(GamingExperienceType))]
+        public GamingExperienceType CurrentGamingExperience { get; set; }
+
+        [TempData] public string StatusMessage { get; set; }
+
+        [BindProperty] public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+
+            [Display(Name = "Change Gaming Experience")]
+            [EnumDataType(typeof(GamingExperienceType))]
+            public GamingExperienceType GamingExperience { get; set; }
+
+            [Required]
+            [StringLength(40, ErrorMessage = "The {0} must be between {2} and {1} characters long.", MinimumLength = 4)]
+            [Display(Name = "Discord Username")]
+            public string DiscordUsername { get; set; }
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+            CurrentGamingExperience = user.GamingExperience;
+
+            Input = new InputModel
+            {
+                PhoneNumber = phoneNumber,
+                DiscordUsername = user.DiscordUsername,
+                GamingExperience = user.GamingExperience,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            var id = await _usersService.EditProfileAsync(user.Id, Input.DiscordUsername, Input.GamingExperience);
+            if (id == GlobalConstants.NotFound)
+            {
+                return this.NotFound();
+            }
+
+            var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    var userId = await _userManager.GetUserIdAsync(user);
+                    throw new InvalidOperationException(
+                        $"Unexpected error occurred setting phone number for user with ID '{userId}'.");
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
+        }
+    }
+}
