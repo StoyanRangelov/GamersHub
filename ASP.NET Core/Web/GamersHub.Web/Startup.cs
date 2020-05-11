0a498a6683daf4f2cf5c60672d49d29b63f2013a
StoyanRangelov <49279325+StoyanRangelov@users.noreply.github.com>
2020-05-11 11:31:16 +0300

    Added chaining for appsettings.json, Development and Production files

--- ASP.NET Core/Web/GamersHub.Web/Startup.cs
+++ ASP.NET Core/Web/GamersHub.Web/Startup.cs
@@ -44,6 +44,14 @@
         public Startup(IConfiguration configuration)
         {
             this.configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.Development.json", true)
+                .AddJsonFile("appsettings.Production.json", true);
+
+            this.configuration = builder.Build();
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
